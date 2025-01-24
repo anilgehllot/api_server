a2591a76e73ef12c29d6dfae52276510e931db41
anilgehllot <anilgehlot.edu2k4@gmail.com>
2025-01-24 11:52:26 +0530

    mak1

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 });
 
 // Server Listener
-const PORT = 3000;
+const PORT = 9090;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
